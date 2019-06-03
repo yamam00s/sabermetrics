96329f7c051b4db2fe22fd77659c437f3d29078c
yamam00s <s.yamamo0327@gmail.com>
2019-06-04 01:27:23 +0900

    add firebase config

---
+++ plugins/firebase.js
@@ -0,0 +1,15 @@
+// Firebase App is always required and must be first
+import firebase from 'firebase/app'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyB4xGmQ3a-UTP-cBCxg83NEHOaZa64Ck9Q',
+  authDomain: 'sabermetrics-7a6fc.firebaseapp.com',
+  databaseURL: 'https://sabermetrics-7a6fc.firebaseio.com',
+  projectId: 'sabermetrics-7a6fc',
+  storageBucket: 'sabermetrics-7a6fc.appspot.com',
+  messagingSenderId: '812791948626',
+  appId: '1:812791948626:web:7a0021a339c63df5'
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
